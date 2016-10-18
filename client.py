0c59dbdaa20b6994dc3bc7635793516b24f00f4d
Felipe Sandoval <felipesandoval@MacBookPro-de-Felipe.local>
2016-10-19 01:33:22 +0200

    ejer5

--- client.py
+++ client.py
@@ -8,22 +8,20 @@ Programa cliente UDP que abre un socket a un servidor
 import socket
 import sys
 
-# Constantes. Dirección IP del servidor y contenido a enviar
-SERVER = sys.argv[1]
-PORT = int(sys.argv[2])
-LINE = ' '.join(sys.argv[3:])
+try:
+    SERVER = sys.argv[1]
+    PORT = int(sys.argv[2])
+    LINE = ' '.join(sys.argv[3:])
+except IndexError:
+    sys.exit("Usage: python3 client.py ip port line")
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 if __name__ == "__main__":
-    try:
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-            my_socket.connect((SERVER, PORT))
-            print("Enviando:", LINE)
-            my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-            data = my_socket.recv(1024)
-            print('Recibido -- ', data.decode('utf-8'))
+     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+        my_socket.connect((SERVER, PORT))
+        print("Enviando:", LINE)
+        my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+        data = my_socket.recv(1024)
+        print('Recibido -- ', data.decode('utf-8'))
 
         print("Socket terminado.")
-    except IncompleteError:
-        print("Usage: python3 client.py ip port line")
-        
