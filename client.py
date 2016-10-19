0ed936de1206b20b6fc618f6296787d1c0067d10
Felipe Sandoval <felipesandoval@MacBookPro-de-Felipe.local>
2016-10-19 13:44:39 +0200

    terminado ejer7 con pep8 y pep257

--- client.py
+++ client.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # Made by Felipe Sandoval Sibada
-"""
-Programa cliente UDP que abre un socket a un servidor
-"""
+"""Programa cliente UDP que abre un socket a un servidor."""
 
 import socket
 import sys
@@ -12,14 +10,17 @@ try:
     SERVER = sys.argv[1]
     PORT = int(sys.argv[2])
     LINE = ' '.join(sys.argv[3:])
+    if len(sys.argv) != 6 or not str.isdigit(sys.argv[5]):
+        raise IndexError
 except IndexError:
-    sys.exit("Usage: python3 client.py ip port line")
+    sys.exit("Usage: client.py ip puerto REGISTER sip_address expires_value")
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 if __name__ == "__main__":
-     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
         my_socket.connect((SERVER, PORT))
-        print("Enviando:", LINE)
+        print(sys.argv[3] + " " + sys.argv[4], "SIP/2.0\r\n" + "Expires: " +
+              sys.argv[5], "\r\n\r\n")
         my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
         data = my_socket.recv(1024)
         print('Recibido -- ', data.decode('utf-8'))
