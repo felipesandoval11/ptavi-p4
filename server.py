31be06f8c985895a9e12f68e9270049caf7416d3
Felipe Sandoval <felipesandoval@MacBookPro-de-Felipe.local>
2016-10-20 02:25:36 +0200

    ya imprimo un json parecido al deseado... aun falta

--- server.py
+++ server.py
@@ -4,6 +4,9 @@
 """Programa servidor UDP que abre un socket a un servidor."""
 
 import socketserver
+import json
+import datetime
+import time
 import sys
 
 
@@ -11,6 +14,25 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """Defino mi variable my_dic como atributo de clase."""
 
     my_dic = {}
+    my_user = []
+    
+    def json2registered(self):
+        try:
+            with open("registered.json", "r") as data_file:
+                data = json.load(data_file)
+            return data
+        except FileNotFoundError:
+            return False
+        
+    
+    def register2json(self):
+        if not self.json2registered():
+            with open("registered.json", "w") as outfile:
+                json.dump([self.my_user],  outfile, indent=4, sort_keys=True,
+                          separators=(',', ':'))
+            print("escribo primero")
+        else:
+            print("actualizo yes")
 
     def handle(self):
         """Handler que se ejecuta cada vez que se reciba un mensaje."""
@@ -21,12 +43,17 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             line_str = str(line.decode('utf-8'))
             sip_addr = line_str.split(' ')[1]
             time_exp = int(line_str.split(' ')[2])
+            time_to_del = time.strftime("%Y-%m-%d %H:%M:%S",
+                          time.gmtime(time.time() + time_exp))
             print("El cliente nos manda ", line_str)
             if line_str.split(' ')[0].isupper()\
                and line_str.split(' ')[0] == "REGISTER":
-                self.my_dic[sip_addr] = str(self.client_address[0])
+                self.my_dic = {"address":str(self.client_address[0]),\
+                               "expires":time_to_del}
+                self.my_user = [sip_addr, self.my_dic]
                 if time_exp == 0:
-                    del self.my_dic[sip_addr]
+                    del self.my_dic["address", "expires"]
+        self.register2json()
 
 if __name__ == "__main__":
     try:
@@ -37,5 +64,4 @@ if __name__ == "__main__":
     except IndexError:
         print("Usage: python3 server.py port")
     except KeyboardInterrupt:
-        print(SIPRegisterHandler.my_dic)
         print("Finalizado servidor")
