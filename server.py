7d3d8baf16c33b758af6ca067b1b28dde7682aae
Felipe Sandoval <felipesandoval@MacBookPro-de-Felipe.local>
2016-10-24 18:37:09 +0200

    optimizado en ingles + todos los peps

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # Made by Felipe Sandoval Sibada
-"""Clase (y programa principal) para un servidor de eco en UDP-SIP."""
+"""Class and main program for a echo server of UDP-SIP."""
 
 import socketserver
 import json
@@ -11,13 +11,13 @@ import sys
 
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
-    """Defino mi variable my_dic como atributo de clase."""
+    """Main handler that use a Dict to save all info."""
 
     my_dic = {}         # Diccionario de clientes para gestionar.
     exist_file = True
 
     def json2registered(self):
-        """Metodo que inspecciona si existe un archivo .json."""
+        """Method to look if there is an over-existing json file."""
         try:
             with open("registered.json", "r") as data_file:
                 self.my_dic = json.load(data_file)
@@ -26,22 +26,21 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             self.exist_file = False
 
     def register2json(self):
-        """Metodo con el que escribo mis clientes en archivo .json."""
-        if not self.exist_file:
+        """Method to write a json file of my_dic."""
+        if not self.exist_file or len(self.my_dic) == 0:
             with open("registered.json", "w") as outfile:
                 json.dump(self.my_dic,  outfile, indent=4, sort_keys=True,
                           separators=(',', ':'))
         else:
-            self.expired()
             with open("registered.json", "r+") as outfile:
                 json.dump(self.my_dic,  outfile, indent=4, sort_keys=True,
                           separators=(',', ':'))
 
     def handle(self):
-        """Handler que se ejecuta cada vez que se reciba un mensaje."""
+        """Handler to manage incoming users SIP request."""
         found = False
-        print("IP cliente:" + str(self.client_address[0]),
-              "| puerto: " + str(self.client_address[1]), '\n')
+        print("IP:" + str(self.client_address[0]),
+              "| PORT: " + str(self.client_address[1]), '\n')
         line_str = self.rfile.read().decode('utf-8')
         sip_addr = line_str.split(' ')[1][4:]
         time_exp = int(line_str.split(' ')[3])
@@ -55,11 +54,12 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                                      "expires": time_to_del}
             if time_exp == 0:
                 del self.my_dic[sip_addr]
+            self.expired()
             self.register2json()
             self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
 
     def expired(self):
-        """Gestiona el tiempo de expiracion de los usuarios."""
+        """Method that checks if there's an old client in my_dic."""
         actual_time = time.strftime("%Y-%m-%d %H:%M:%S",
                                     time.gmtime(time.time()))
         expired_dic = []
@@ -75,9 +75,9 @@ if __name__ == "__main__":
     try:
         serv = socketserver.UDPServer(('', int(sys.argv[1])),
                                       SIPRegisterHandler)
-        print("Lanzando servidor UDP de eco...")
+        print("Lauching UDP echo server...")
         serv.serve_forever()
     except IndexError:
         sys.exit("Usage: python3 server.py port")
     except KeyboardInterrupt:
-        print("Finalizado servidor")
+        print("END OF SERVER")
